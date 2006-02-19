87b229fbc192f557bf4904128d44019b883f2804
dado1945 <dado1945@f7c98141-a1f8-0310-8aa8-ca843b615e60>
2006-02-19 17:58:40 +0000

    some additions

--- gnutri2/gnutri2/controllers.py
+++ gnutri2/gnutri2/controllers.py
@@ -1,9 +1,34 @@
 import turbogears
 from turbogears import controllers
 
+class Edit:
+
+    @turbogears.expose(html="gnutri2.templates.edit")
+    def index(self, page=''):
+        return dict(name=page, description="testas")
+
+    @turbogears.expose()
+    def default(self, pagename):
+        return self.index(pagename)
+
+class Search:
+
+    @turbogears.expose(html="gnutri2.templates.search")
+    def index(self, keyword=''):
+        products = ['1', '2', '3', keyword]
+        return dict(products=products)
+
+    @turbogears.expose()
+    def default(self, pagename):
+        return self.index(pagename)
+
+
 class Root(controllers.Root):
 
-    @turbogears.expose(html="gnutri2.templates.welcome")
+    edit = Edit()
+    search = Search()
+
+    @turbogears.expose(html="gnutri2.templates.index")
     def index(self):
         import time
         return dict(now=time.ctime())
--- gnutri2/gnutri2/templates/master.kid
+++ gnutri2/gnutri2/templates/master.kid
@@ -4,15 +4,20 @@
     xmlns="http://www.w3.org/1999/xhtml"
     xmlns:py="http://purl.org/kid/ns#"
     py:extends="sitetemplate">
-<head>
-    <meta
-        content="text/html; charset=UTF-8"
-        http-equiv="content-type" py:replace="''"/>
-    <title></title>
-</head>
+
+    <head py:match="item.tag=='{http://www.w3.org/1999/xhtml}head'">
+        <meta content="text/html; charset=UTF-8"
+            http-equiv="content-type" py:replace="''"/>
+        <title py:replace="''">Your title goes here</title>
+        <meta py:replace="item[:]"/>
+        <script src="/tg_js/MochiKit.js"></script>
+    </head>
 
 <body py:match="item.tag=='{http://www.w3.org/1999/xhtml}body'">
     &gt;&gt;GNUtri
+
+    <div py:if="tg_flash" class="flash" py:content="tg_flash"></div>
+
     <div py:replace="item[:]"/>
     <hr/>
     dado1945 &copy;
